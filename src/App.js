73ac148e0db9c0cfccb464a1ed6b1d3ce0ff4348
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-09-30 18:40:14 +1000

    created crypto app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from "react";
+import axios from "axios";
 import './App.css';
+import Coin from "./Coin";
+
+
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+      .then((res) => {
+        setCoins(res.data);  
+      })
+      .catch(err => {
+      console.log(err);
+    })
+  }, [])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => (
+    coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase())
+  ))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="d-flex justify-content-between align-items-center p-2">
+        <h1>Coingecko App</h1>
+        <form>
+          <input type="text" placeholder="search coin" className="coin_input" onChange={ handleChange}/>
+        </form>
+      </div>
+
+        <Coin coins={filteredCoins}/>
+      
     </div>
   );
 }
