6f59e244d097400c6bd389f8d8d3355df6188ed0
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-10-01 14:34:56 +1000

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,43 +1,69 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
-import Coin from "./Coin";
-
-
+import "./App.css";
+import Coin from "./components/Coin";
+import Navbar from "./components/Navbar";
+import Pagination from "./components/Pagination";
+// import CoinsTable from "./components/CoinsTable"
 
 function App() {
-
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [coinsPerPage, setCoinsPerPage] = useState(10);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
-      .then((res) => {
-        setCoins(res.data);  
-      })
-      .catch(err => {
-      console.log(err);
-    })
-  }, [])
+    const fetchCoins = async () => {
+      try {
+        setLoading(true);
+        //make an api call
+        const res = await axios.get(
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+        );
+        setCoins(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCoins();
+  }, []);
 
   const handleChange = (e) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
-  const filteredCoins = coins.filter(coin => (
-    coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase())
-  ))
+  const filteredCoins = coins.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(search.toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(search.toLowerCase())
+  );
+
+  //limiting display coins per page
+  const lastCoinIndex = currentPage * coinsPerPage;
+  const firstCoinIndex = lastCoinIndex - coinsPerPage;
+  const currentCoins = filteredCoins.slice(firstCoinIndex, lastCoinIndex);
+
+  //handleClick for pagination
+  const handleClick = (pageNumber) => {
+    if (pageNumber === "previous") {
+      setCurrentPage(currentPage - 1);
+    }
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div className="App">
-      <div className="d-flex justify-content-between align-items-center p-2">
-        <h1>Coingecko App</h1>
-        <form>
-          <input type="text" placeholder="search coin" className="coin_input" onChange={ handleChange}/>
-        </form>
-      </div>
-
-        <Coin coins={filteredCoins}/>
-      
+    <div className="container-fluid text-center">
+      <Navbar handleChange={handleChange} />
+      {/* <CoinsTable coins={filteredCoins}/> */}
+      <Pagination
+        coinsPerPage={coinsPerPage}
+        totalCoins={filteredCoins.length}
+        paginate={handleClick}
+      />
+      <Coin coins={currentCoins} loading={loading} />
     </div>
   );
 }
